bbe11c4e03285e3c8504fb48ae6dfe3fb4688ab9
Kacper Żabiński <67337054+Hashtag-Easy@users.noreply.github.com>
2024-01-08 16:51:48 +0100

    Init

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
+</head>
+<body>
+    <h2>Zalogowano</h2>
+    Przykładowa zawartość strony
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username_db = "root";
+$password_db = "";
+$database = "test";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlentities($_POST["username"]);
+    $password = htmlentities($_POST["password"]);
+
+    $conn = new mysqli($servername, $username_db, $password_db, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $query = "SELECT * FROM users WHERE NAME = '$username' AND PASSWORD = '$password'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo "Invalid username or password";
+    }
+
+    $conn->close();
+}
